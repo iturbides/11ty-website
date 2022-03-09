9f1f40b31375605478e6c1bfcd701b90943d1fb1
Iturbides <iturbides@keemail.me>
2022-03-09 18:52:06 +0100

    Create pardeletras.js

---
+++ src/_data/sites/pardeletras.js
@@ -0,0 +1,8 @@
+{
+    "url": "https://pardeletras.com",
+    "name": "Pardeletras",
+    "description": "A personal blog",
+    "twitter": "",
+    "authoredBy": ["Matias"],
+    "source_url": ""
+}
